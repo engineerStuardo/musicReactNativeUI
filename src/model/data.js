a19edc2f60aeec156bb0f56a2c92094fdca1836d
icortez <icortez@ufg.edu.sv>
2021-07-27 17:10:47 -0600

    add images and build array in datajs file

---
+++ src/model/data.js
@@ -0,0 +1,34 @@
+const songs = [
+  {
+    id: '1',
+    title: 'Kontho',
+    artist: 'Anupam Roy',
+    image: require('../assets/artwork/Kontho.jpg'),
+  },
+  {
+    id: '2',
+    title: 'Humraah',
+    artist: 'Sachet Tandon',
+    image: require('../assets/artwork/Humraah.jpg'),
+  },
+  {
+    id: '3',
+    title: 'Kyon',
+    artist: 'B Praak, Paypal Dev',
+    image: require('../assets/artwork/Kyon.jpg'),
+  },
+  {
+    id: '4',
+    title: 'Buzz',
+    artist: 'Aastha Gill, Badshah',
+    image: require('../assets/artwork/Buzz.jpg'),
+  },
+  {
+    id: '5',
+    title: 'Sunflower',
+    artist: 'Post Malone, Swae Lee',
+    image: require('../assets/artwork/Sunflower.jpg'),
+  },
+];
+
+export default songs;
