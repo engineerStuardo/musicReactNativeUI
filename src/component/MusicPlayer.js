b8ebc3feb53f7beae4f34e28c22e2d5554d8f134
icortez <icortez@ufg.edu.sv>
2021-07-27 17:46:17 -0600

    install vector icons and slider and also configure it in ios and android with link

--- src/component/MusicPlayer.js
+++ src/component/MusicPlayer.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Slider from '@react-native-community/slider';
 
 const MusicPlayer = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Ionicons name="heart-outline" size={30} />
       <Text>hello from music player component</Text>
+      <Slider
+        style={{width: 200, height: 40}}
+        minimumValue={0}
+        maximumValue={1}
+        minimumTrackTintColor="#FFFFFF"
+        maximumTrackTintColor="#000000"
+      />
     </View>
   );
 };
